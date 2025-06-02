8778d8ddc754927c44f50142a840e208fe1a30c8
yassimin Momed <89214962+momed003@users.noreply.github.com>
2025-06-02 17:08:43 +0200

    create suplier

---
+++ routes/supplier.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import { admin, db } from '../config/Connecion.js';
+
+export const createSupplier=async(req,res)=>{
+    const supplier=req.body;
+
+    if (supplier.name==null||supplier.email==null||supplier.phone==null||supplier.nuit==null|| supplier.address==null || supplier.password==null) {
+        return res.status(400).json({error:'All fields are required'});
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@greengrocer\.mz$/;
+    if (!emailRegex.test(supplier.email.toLowerCase())) {
+        return res.status(400).json({
+            error: 'Invalid email address',
+            message: 'only @greengrocer.mz emails are allowed'
+        });
+    }
+
+    try {
+        const supplierRef=db.collection('suppliers');
+        const supplierSnapshot=await supplierRef.where('email', '==', supplier.email).get();
+        if (!supplierSnapshot.empty) {
+            return res.status(400).json({error:'Email already in use'});
+        }
+
+        const newSupplier=({
+            name:supplier.name,
+            email: supplier.email,
+            password: supplier.password,
+            phone: supplier.phone,
+            nuit: supplier.nuit,
+            adress: supplier.address,
+            createdAt: admin.firestore.FieldValue.serverTimestamp()
+        })
+
+        const supplierDocRef=await db.collection('suppliers').add(newSupplier);
+        return res.status(201).json({
+            message:'Supplier created successfully!',
+            supplierId: supplierDocRef.id
+        });
+
+    }catch (e) {
+        console.error('Error creating supplier:', e);
+        return res.status(500).json({ error: 'Internal error! Server not avaiable' });
+    }
+
+};
+const router = express.Router();
+
+router.post('/create-supplier', createSupplier);
+
+export default router;
