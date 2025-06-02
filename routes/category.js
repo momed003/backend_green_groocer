1d515d977bcda8a3502962d0b241f846560bbeba
yassimin Momed <89214962+momed003@users.noreply.github.com>
2025-06-02 20:26:53 +0200

    update routs

---
+++ routes/category.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { admin, db } from '../config/Connecion.js';
+
+export const createCategory=async(req,res)=>{
+    const category=req.body;
+    if (category.name==null) {
+        return res.status(400).json({error:'Name is required'});
+    }
+
+    try {
+
+        const categoryRef=db.collection('categories');
+        const categorySnapshot=await categoryRef.where('name', '==', category.name).get();
+        if (!categorySnapshot.empty) {
+            return res.status(400).json({error:'Category already exists'});
+        }
+
+        const newCategory=({
+            name:category.name,
+            createdAt: admin.firestore.FieldValue.serverTimestamp()
+        })
+        const categoryDocRef=await db.collection('categories').add(newCategory);
+        return res.status(201).json({
+            message:'Category created successfully!',
+            categoryId: categoryDocRef.id
+        });
+    }catch (e) {
+        console.error('Error creating category:', e);
+        return res.status(500).json({ error: 'Internal error! Server not avaiable' });
+    }
+
+};
+
+const router = express.Router();
+router.post('/create-category', createCategory);
+
+export default router;
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import router from './routes/Register.js';
 import loginRouter from './routes/Login.js';
+import createSupplier from './routes/supplier.js';
+import createProduct from './routes/product.js';
+import createCategory from './routes/category.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', router);
 app.use('/api', loginRouter);
-
+app.use('/api', createSupplier);
+app.use('/api', createProduct);
+app.use('/api', createCategory);
 
 const PORT = 3702 ;
 app.listen(PORT, () => {
