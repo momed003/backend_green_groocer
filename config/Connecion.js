9f4b3d615d2b0ba90fed67dd71637904cd434af9
yassimin Momed <89214962+momed003@users.noreply.github.com>
2025-06-09 02:36:34 +0200

    config project

--- config/Connecion.js
+++ config/Connecion.js
@@ -1,16 +1,13 @@
-// firebase.js
-const fs = require('fs');
-const path = require('path');
+
+require('dotenv').config();
 const admin = require('firebase-admin');
 
-/*const serviceAccount = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '../adminsdk.json'), 'utf-8')
-);*/
-const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+const rawCreds = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+rawCreds.private_key = rawCreds.private_key.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(rawCreds)
     });
 }
 
