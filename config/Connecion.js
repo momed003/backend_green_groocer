79da844a05b16ad7ebd1e550185f26432ce69322
yassimin Momed <89214962+momed003@users.noreply.github.com>
2025-06-09 02:23:36 +0200

    send to render

--- config/Connecion.js
+++ config/Connecion.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(
+/*const serviceAccount = JSON.parse(
     fs.readFileSync(path.join(__dirname, '../adminsdk.json'), 'utf-8')
-);
+);*/
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
     });
-}git add config/Connecion.jsgit
+}
 
 const db = admin.firestore();
 
